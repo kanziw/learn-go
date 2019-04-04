1f80a4b90c1f775cd12c0e85fe9977cfc7287fd8
kanziw <kanziwoong@gmail.com>
2019-04-04 18:47:06 +0900

    Add channel buffering examples

--- 00_grammar/17_channels.go
+++ 00_grammar/17_channels.go
@@ -6,7 +6,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
@@ -17,11 +20,15 @@ func main() {
 	// _Send_ a value into a channel using the `channel <-`
 	// syntax. Here we send `"ping"`  to the `messages`
 	// channel we made above, from a new goroutine.
-	go func() { messages <- "ping" }()
+	go func() {
+		messages <- "ping"
+		time.Sleep(2 * time.Second)
+		messages <- "pong"
+	}()
 
 	// The `<-channel` syntax _receives_ a value from the
 	// channel. Here we'll receive the `"ping"` message
 	// we sent above and print it out.
-	msg := <-messages
-	fmt.Println(msg)
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
 }
